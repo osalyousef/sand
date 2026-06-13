f69c4e93ac346aa1af512955d6982b66741abc7d
Marwan Fahad <mamemo1270@gmail.com>
2026-06-13 21:09:27 +0300

    cleanup: remove old dashboard directory in favor of dashboard2

--- dashboard/next.config.ts
+++
@@ -1,7 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  allowedDevOrigins: ["scott-jet-shirts-relatively.trycloudflare.com"],
-};
-
-export default nextConfig;
